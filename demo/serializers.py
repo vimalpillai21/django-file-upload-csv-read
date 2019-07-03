dff87bead1cf73df491813c4b07400df8b0f2f1f
Vimal-P <vimalpillai21@gmail.com>
2019-07-03 17:22:34 +0530

    decorator used

--- demo/serializers.py
+++ demo/serializers.py
@@ -2,3 +2,7 @@ from rest_framework import serializers
 
 class FileSerializer(serializers.Serializer):
     file = serializers.FileField()
+
+class RegisterSerializer(serializers.Serializer):
+    username = serializers.CharField(max_length=120,required=True)
+    email = serializers.EmailField(required=True)
--- demo/views.py
+++ demo/views.py
@@ -2,13 +2,28 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import FileSerializer
+from django.views.decorators.csrf import csrf_exempt
+from functools import wraps
 from rest_framework.parsers import FileUploadParser
-import csv,io
+import csv
+from django.contrib.auth.models import User
 # Create your views here.
+
+def my_decorator(func):
+    def wrapper(*args,**kwargs):
+        print(args)
+        print("Before executing get method")
+        print(args[1].user)
+        return func(*args,**kwargs)
+        print("After executing get method")
+    return wraps(func)(wrapper)
+
 class DemoApiView(APIView):
     serializer_class = FileSerializer
     parser_class = (FileUploadParser,)
-    def get(self,request,*args,**kwargs):
+
+    @my_decorator
+    def get(self,request):
         return Response({
             'data':{
                 'message': 'success'
@@ -17,16 +32,18 @@ class DemoApiView(APIView):
 
     def post(self,request,*args,**kwargs):
         try:
-            print(request.data)
-            file = request.data.get('file')
-            print(file.temporary_file_path())
-            print(file.content_type)
-            with open(file.temporary_file_path(),'r+') as f:
-                readed_message = csv.reader(f)
-                for message in readed_message:
-                    print(message)
-            return Response({'data': "success" })
+            serializer = FileSerializer(data=request.data)
+            if serializer.is_valid():
+                file = serializer.validated_data.get('file')
+                print(file.temporary_file_path())
+                print(file.content_type)
+                with open(file.temporary_file_path(),'r+') as f:
+                    readed_message = csv.reader(f)
+                    for message in readed_message:
+                        print(message)
+                return Response({'data': "success" })
+            else:
+                return Response(serializer.errors)
         except Exception as e:
-            print('hello')
             print(str(e))
-            return Response({'data': "failed" })
+            return Response({'data': str(e)})
